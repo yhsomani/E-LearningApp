a422bd43d00891bc6476a288992e97f9c7219b17
LoneWolf <saurabhdalakoti07@gmail.com>
2019-10-13 01:20:31 +0530

    first PR and GUI for app

---
+++ app/src/main/java/androiddev/com/elearning/MainActivity.java
@@ -0,0 +1,34 @@
+package androiddev.com.elearning;
+
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.AppBarLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TableLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private ActionBarDrawerToggle mToogle;
+    private DrawerLayout mDrawerLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDrawerLayout=(DrawerLayout) findViewById(R.id.drawer);
+        mToogle=new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToogle);
+        mToogle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(mToogle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
